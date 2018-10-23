0a9a1e693fbd4cd2ea3257599840d598cbea1ec9
Travis CI <tanner.barlow12@gmail.com>
2018-10-23 14:51:08 -0700

    remove extra space between tests

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -10,14 +10,12 @@ class TestFilter(TestCase):
         f = resource_service.get_filter(None)
         self.assertEqual(f.normalized_filter(), None)
 
-
     def test_shorthand_filter(self):
         resource_service = AzureResourceService(None)
         f = resource_service.get_filter('vm')
         self.assertEqual(f.normalized_filter(), "resourceType eq 'Microsoft.Compute/virtualMachines'")
         f = resource_service.get_filter('storage')
         self.assertEqual(f.normalized_filter(), "resourceType eq 'Microsoft.Storage/storageAccounts'")
-
 
     def test_random_azure_resource_filter(self):
         resource_type = 'Microsoft.Network/applicationGateways'
